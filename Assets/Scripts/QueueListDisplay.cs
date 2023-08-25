95999330d48ad2a471823b2e0a33cd449b484bec
Tim Traver <timtraver@gmail.com>
2023-08-25 01:33:25 -0700

    Added double click

--- Assets/Scripts/QueueListDisplay.cs
+++ Assets/Scripts/QueueListDisplay.cs
@@ -10,6 +10,7 @@ public class QueueListDisplay : MonoBehaviour
     public Text queueDescription;
     public PlayQueueEntry entry;
     public QueueControl queueControl;
+    public float doubleClickTime = .2f, lastClickTime;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +31,16 @@ public class QueueListDisplay : MonoBehaviour
         queueControl.lastQueueEntry = queueControl.currentQueueEntry;
         queueControl.QueueToSpecificEntry(System.Convert.ToInt32(queueSequence.text) - 1);
         return;
+    }
+    public void onQueueEntryDoubleClick()
+    {
+        // Checking mouse double click
+        float timeSinceLastClick = Time.time - lastClickTime;
+        if (timeSinceLastClick <= doubleClickTime)
+        {
+            onQueueEntryClick();
+        }
+        lastClickTime = Time.time;
     }
 
 }
