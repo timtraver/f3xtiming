44abdf944747e97cd5c2302296aaf6ac6c092be4
Tim Traver <timtraver@gmail.com>
2023-08-25 01:25:20 -0700

    Changed drqw round headers to have round and group

--- Assets/Scripts/DrawRoundDisplay.cs
+++ Assets/Scripts/DrawRoundDisplay.cs
@@ -6,17 +6,18 @@ using UnityEngine.UI;
 public class DrawRoundDisplay : MonoBehaviour
 {
     public Text roundNumber;
+    public Text groupText;
     public EventRound round;
     // Start is called before the first frame update
     void Start()
     {
-        if (round != null) Prime(round);
+        if (round != null) Prime(round, groupText.text);
     }
 
-    public void Prime(EventRound round)
+    public void Prime(EventRound round, string group)
     {
         this.round = round;
         if (roundNumber != null)
-            roundNumber.text = "Round " + round.round_number;
+            roundNumber.text = "Round " + round.round_number + "    " + "Group " + group;
     }
 }
